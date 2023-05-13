2edf051c187a758573f4fedc807f60ff89c783dc
MohamedHalim <mohamedhalim1998@gmail.com>
2023-05-13 20:37:50 +0300

    UPDATE: 11/5/2023

---
+++ src/LeetCode_334.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class LeetCode_334 {
+
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE;
+        int second = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= first) {
+                first = num;
+            } else if (num <= second) {
+                second = num;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+
+
+}
